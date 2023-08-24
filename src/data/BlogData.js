a4b8fbf7a82779a5736bf38374e376fcf84f8f70
Sameer Faridi <s.faridi007@gmail.com>
2023-08-24 23:20:58 +0530

    final commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Data Structure String In JavaScript | Frontend Interview Helper",
+        tags:["JavaScript","DSA","Strings"],
+        date:"Aug 21, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1692611938291/8fdf0d28-a161-43a3-80dc-822c5315536b.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://sameerkali.hashnode.dev/data-structure-string-in-javascript-frontend-interview-helper"
+    },
+    {
+        id:2,
+        name:"learning in public",
+        tags:["webDevelopment","learnInPublic"],
+        date:"Aug 16, 2023",
+        imgSrc:"https://verbaltovisual.com/wp-content/uploads/2023/02/LearnInPublic_Featured.jpg",
+        link:"https://sameerkali.hashnode.dev/learning-in-public"
+    },
+    {
+        id:3,
+        name:"How HTML Files Render in Browser (DOM)",
+        tags:["RENDERING","CSSOM","DOM","BROWSER","DEVTOOLS"],
+        date:"Aug 17, 2023",
+        imgSrc:"https://cdn.hashnode.com/res/hashnode/image/upload/v1692204658833/afdae1b6-73a4-41ea-be14-78ee92528b94.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
+        link:"https://sameerkali.hashnode.dev/how-html-files-render-in-browser-dom"
+    },
+    // {
+    //     id:4,
+    //     name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+    //     tags:["react","redux","framer-motion"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--r5oUDhhT--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/036s7j5b73up7455rdth.png",
+    //     link:"https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1"
+    // },
+    // {
+    //     id:5,
+    //     name:"What is Higher Order Component (HOC) in React JS?",
+    //     tags:["react","reactJS","web-development"],
+    //     date:"3 Feb, 2021",
+    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625295480148%2FlyKj8bpHK.jpeg%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    //     link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+    // },
+    // {
+    //     id:6,
+    //     name:"How to implement Pagination Component in React from scratch",
+    //     tags:["react","css"],
+    //     date:"5 Jan, 2021",
+    //     imgSrc:"https://codebucks.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1625240050668%2FGAya9b0XK.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=1920&q=75",
+    //     link:"https://codebucks.hashnode.dev/pagination-in-react"
+
+    // },
+   
+    // {
+    //     id:7,
+    //     name:"What is Redux ? Simply Explained!",
+    //     tags:["react","redux","javascript"],
+    //     date:"16 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+    //     link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    // },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Indie Gems Portal",
+        description:"🎮 Indie Gems Portal is a cool web app where you can hunt for games using genres, platforms, release dates, and ratings ⭐. It's like a treasure map for finding your kind of games! 🗺️🕹️",
+        tags:["react","game","api","open-source","grid","chakra-ui"],
+        
+        demo:"https://indie-gems-portal.vercel.app/",
+        github:"https://github.com/sameerkali/Indie_Gems_Portal"
+    },
+    {
+        id:2,
+        name:"Sameer's Portfolio",
+        description:"🚀 Explore a cutting-edge Personal Portfolio Project! 🎨 Powered by React 18🌟",
+        tags:["react","javascript","api","opensource"],
+        
+        demo:"https://sameers-portfolio.vercel.app/",
+        github:"https://github.com/sameerkali/Sameers_Portfolio"
+        
+    },
+    {
+        id:3,
+        name:"SpaceVoyage",
+        description:"Experience SpaceVoyage: Cosmic Rocketry, launching rockets to Kepler planets, exploring the cosmos.",
+        tags:["react","javascript","api","opensource","docker","mongodb","node","mern"],
+        
+        demo:"https://github.com/sameerkali/SpaceVoyage",
+        github:"https://github.com/sameerkali/SpaceVoyage"
+    },
+    {
+        id:4,
+        name:"FilmSloth",
+        description:"[developing stage]🕵️‍♂️ FilmSloth: Unlock cinematic secrets! Discover hidden 🎥 gems effortlessly and delve into the world of entertainment with our intuitive app.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://github.com/sameerkali/FilmSloth",
+        github:"https://github.com/sameerkali/FilmSloth"
+       
+    },{
+        id:5,
+        name:"Landing Page For A Developer",
+        description:"🚀 Explore a cutting-edge Personal Portfolio Project! 🎨 Powered by React🌟",
+        tags:["react","redux","node","express", "bootstrap"],
+        
+        demo:"https://tsaransh.netlify.app/#connect",
+        github:"https://github.com/sameerkali/Saranshs_portfolio"
+        
+    },
+    {
+        id:6,
+        name:"LingoFlow",
+        description:"LingoFlow: Effortlessly transcribe speech to text with React 18 magic! Real-time, multilingual, secure. Maximize productivity, creativity, accessibility. Embrace the communication future today! 🚀🗣️📝 #ReaccuVoice",
+        tags:["react","typescript","api","opensource","speech-to-text"],
+        
+        demo:"https://lingo-flow.vercel.app/",
+        github:"https://github.com/sameerkali/LingoFlow"
+        
+    },
+    
+  
+] 
